98d089bc1d46f27164f13fd61094996f5f89b231
rifqee23 <rifqifebrianto746@gmail.com>
2024-07-22 16:42:05 +0700

    latihan blog json

--- react-fundamental/src/components/Article.jsx
+++ react-fundamental/src/components/Article.jsx
@@ -1,16 +1,12 @@
+import '../App.css'
+
 function Article(props) {
     
     return (
         <>
-            <div>{props.name}</div>
-            <div>
-                {
-                    props.title.map(title =>(
-                        <div>- {title}</div>
-                    )
-                    ) 
-                }
-            </div>
+            <h3 className="title">{props.title}</h3>
+            <small>Date : {props.date}, tags : {props.tags.join(", ")}</small>
+            
         </>
     )
 }
