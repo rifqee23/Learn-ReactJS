83dc26436be5e4630122d1440ae1ffc3bc34a51c
rifqee23 <127317331+rifqee23@users.noreply.github.com>
2024-07-22 19:32:14 +0700

    belajar useState (#4)

--- react-fundamental/src/pages/index.jsx
+++ react-fundamental/src/pages/index.jsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import posts from '../posts.json'
 import Article from '../components/Article';
 
 function Homepage() {
+    const [search, setSearch] = useState("");
+    const changeSearch = event => {
+        console.log(event.target.value);
+        setSearch(event.target.value);
+    };
     return(
         <>
             <h1>Simple Blog</h1>
+            <div>Cari Artikel : <input type="search" name="search" id="search" onChange={changeSearch} /></div>
+            <small>Ditemukan 0 kata dengan pencarian kata {search}</small>
             {
-                posts.map( blog => (
-                    <Article title={blog.title} tags={blog.tags} date={blog.date} />
+                posts.map( ({title, tags, date}, index) => (
+                    <Article  key={index} {...{title, tags, date}} />
+
                 ))
             }
         </>
