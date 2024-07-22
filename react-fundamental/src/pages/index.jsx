6e608ed626cf506af6b1b77f17dd47448216230a
rifqee23 <rifqifebrianto746@gmail.com>
2024-07-23 01:16:03 +0700

    belajar mengembalikan value ke parent dgn props

--- react-fundamental/src/pages/index.jsx
+++ react-fundamental/src/pages/index.jsx
@@ -1,21 +1,22 @@
-import { useState } from 'react';
-import posts from '../posts.json'
+import postsData from '../posts.json'
 import Article from '../components/Article';
+import Search from '../components/Search';
+import { useState } from 'react';
 
 function Homepage() {
-    const [search, setSearch] = useState("");
-    const changeSearch = event => {
-        console.log(event.target.value);
-        setSearch(event.target.value);
-    };
+    const [posts, setPosts] = useState(postsData);
+    const onSearchChange = (value) => {
+        const filterPosts = postsData.filter(item => item.title.toLocaleLowerCase().includes(value));
+        setPosts(filterPosts);
+    }
+    
     return(
         <>
             <h1>Simple Blog</h1>
-            <div>Cari Artikel : <input type="search" name="search" id="search" onChange={changeSearch} /></div>
-            <small>Ditemukan 0 kata dengan pencarian kata {search}</small>
+            <Search onSearchChange={onSearchChange}/>
             {
-                posts.map( ({title, tags, date}, index) => (
-                    <Article  key={index} {...{title, tags, date}} />
+                posts.map( (props, index) => (
+                    <Article  key={index} {...props} />
 
                 ))
             }
