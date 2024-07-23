4b1672cd43871fc5bf65cdf4365b3a761e464b94
rifqee23 <127317331+rifqee23@users.noreply.github.com>
2024-07-23 19:57:50 +0700

    belajar react-router (#12)

--- react-fundamental/src/App.jsx
+++ react-fundamental/src/App.jsx
@@ -1,6 +1,8 @@
-import './App.css'
-import Home from './pages/index'
-import { GlobalContext } from './context'
+import './App.css';
+import Home from './pages/index';
+import { GlobalContext } from './context';
+import { RouterProvider } from 'react-router-dom';
+import {router} from './routers';
 function App() {
   const user = {
     username : 'Rifqi Febrianto'
@@ -8,7 +10,7 @@ function App() {
   return (
     <>
     <GlobalContext.Provider value={user}>
-      <Home />
+      <RouterProvider router={router}/>
     </GlobalContext.Provider>
       
     </>
