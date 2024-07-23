3d8170e58742a84fde39240069b70a0495366008
rifqee23 <127317331+rifqee23@users.noreply.github.com>
2024-07-23 22:54:37 +0700

    halaman about (#13)

--- react-fundamental/src/routers/index.jsx
+++ react-fundamental/src/routers/index.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from '../pages/index'
+import Home from '../pages/index';
+import About from "../pages/about";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Home/>,
+    },
+    {
+        path: '/about',
+        element: <About/>,
     }
 ])
