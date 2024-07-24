e8e86c1beab540147774dd6f44d9150aeada5797
rifqee23 <127317331+rifqee23@users.noreply.github.com>
2024-07-24 12:11:34 +0700

    layouting (#14)

--- react-fundamental/src/components/Article.jsx
+++ react-fundamental/src/components/Article.jsx
@@ -1,32 +1,31 @@
-import '../App.css'
-import { useContext } from 'react';
-import { GlobalContext } from '../context';
+import "../App.css";
+import { useContext } from "react";
+import { GlobalContext } from "../context";
 
 // component article status
-const ArticleStatus = (({ isNew }) => (isNew && <span>-- Baru !!</span> ));
+const ArticleStatus = ({ isNew }) => isNew && <span>-- Baru !!</span>;
 
 // component new article
-const NewArticle = () => (<span> -- Baru !!!</span>)
+const NewArticle = () => <span> -- Baru !!!</span>;
 
 function Article(props) {
-    const user = useContext(GlobalContext);
-    
-    return (
-        <>
-            <h3 className="title">{props.title}</h3>
-            <small>
-                Date : {props.date}, tags : {props.tags.join(", ")} 
-                {/* {props.isNew ? " --Baru " : "lama"} */}
-                {/* {props.isNew && " --Baru "} */} 
-                <ArticleStatus isNew={props.isNew} /> {/*Pengecekan didalam component*/}
-                {/* {props.isNew && <NewArticle />} Pengecekan diluar component */}
-            </small>
-            <div>
-                <small>Ditulis oleh {user.username}</small>
-            </div>
-            
-        </>
-    )
+  const user = useContext(GlobalContext);
+
+  return (
+    <>
+      <h3 className="title">{props.title}</h3>
+      <small>
+        Date : {props.date}, tags : {props.tags.join(", ")}
+        {/* {props.isNew ? " --Baru " : "lama"} */}
+        {/* {props.isNew && " --Baru "} */}
+        <ArticleStatus isNew={props.isNew} /> {/*Pengecekan didalam component*/}
+        {/* {props.isNew && <NewArticle />} Pengecekan diluar component */}
+      </small>
+      <div>
+        <small>Ditulis oleh {user.username}</small>
+      </div>
+    </>
+  );
 }
 
 export default Article;
