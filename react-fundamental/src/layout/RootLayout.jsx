db6592da7ee1f355771eab8d682c98602850a56c
rifqee23 <rifqifebrianto746@gmail.com>
2024-07-24 19:57:33 +0700

    belajar dynamic styling

--- react-fundamental/src/layout/RootLayout.jsx
+++ react-fundamental/src/layout/RootLayout.jsx
@@ -1,10 +1,34 @@
-import { Link, Outlet } from "react-router-dom";
-
+import { Link, NavLink, Outlet } from "react-router-dom";
+import "../styles/index.css";
 function RootLayout() {
   return (
     <>
-      <Link to={"/"}>Home</Link> | <Link to={"/about"}>About</Link> |{" "}
-      <Link to={"/blog"}>Blog</Link>
+      <NavLink
+        className={({ isActive, isPending }) =>
+          isActive ? "active" : isPending ? "pending" : ""
+        }
+        to={"/"}
+      >
+        Home
+      </NavLink>
+      |
+      <NavLink
+        className={({ isActive, isPending }) =>
+          isActive ? "active" : isPending ? "pending" : ""
+        }
+        to={"/about"}
+      >
+        About
+      </NavLink>{" "}
+      |
+      <NavLink
+        className={({ isActive, isPending }) =>
+          isActive ? "active" : isPending ? "pending" : ""
+        }
+        to={"/blog"}
+      >
+        Blog
+      </NavLink>
       <Outlet />
     </>
   );
